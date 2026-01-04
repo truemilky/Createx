c0e23287f78da60567a8cec82632da85de014f88
truemilky <truemilky@gmail.com>
2026-01-04 22:29:07 +0100

    Benefits block, events block, certificate block are done.

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,48 @@ horizontalCardsShowButton.addEventListener("click", function () {
     el.classList.add("show");
   });
 });
+
+/* Benefits Tabs */
+const benefitsTabsButtons = document.querySelectorAll(".benefits-tab-button");
+const benefitsTabsContent = document.querySelectorAll(".benefits-tab-content");
+
+benefitsTabsButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    benefitsTabsButtons.forEach((b) => b.classList.remove("active"));
+    benefitsTabsContent.forEach((c) => c.classList.remove("active"));
+
+    btn.classList.add("active");
+    document.getElementById("tab-" + btn.dataset.tab).classList.add("active");
+  });
+});
+
+/* Events (index.html) "View More" button content */
+const eventCardsButtons = document.querySelectorAll(".event-card-button");
+const eventCardsText = document.querySelectorAll(".event-card-text");
+
+eventCardsButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const activeText = document.getElementById("event-" + btn.dataset.event);
+
+    activeText.classList.toggle("active");
+
+    activeText.classList.contains("active")
+      ? (btn.innerHTML = `<span>Hide</span>`)
+      : (btn.innerHTML = "<span>View more</span>");
+  });
+});
+
+/* Team section (index.html) swiper control */
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 10,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
